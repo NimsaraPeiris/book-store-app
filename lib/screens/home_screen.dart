7ef0d0e929310ba0b6396f631331fade4eb6416c
NiMsArA <pasindunp917@gmail.com>
2025-01-02 16:20:47 +0530

    prices updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,56 +8,56 @@ class HomeScreen extends StatelessWidget {
       id: '1',
       title: 'Dark Hope',
       author: 'H.D. Smith',
-      price: 19.99,
+      price: 1899.99,
       img: './assets/covers/book1.jpeg'
     ),
     Book(
       id: '2',
       title: 'Harry Potter and The Philosopher\'s Stone',
       author: 'J. K. Rowling',
-      price: 14.99,
+      price: 1499.99,
       img: './assets/covers/book8.webp'
     ),
     Book(
       id: '3',
       title: 'The Outer Space',
       author: 'Kimberly Hopkins',
-      price: 16.99,
+      price: 1699.99,
       img: './assets/covers/book3.png'
     ),
     Book(
       id: '4',
       title: 'Words of Radiance',
       author: 'Brandon Sanderson',
-      price: 16.99,
+      price: 1289.99,
       img: './assets/covers/book4.jpg'
     ),
     Book(
       id: '5',
       title: 'The Court of Sinners & Snakes',
       author: 'Hayley Rose',
-      price: 16.99,
+      price: 1399.99,
       img: './assets/covers/book5.jpg'
     ),
     Book(
       id: '6',
       title: 'OMEGA VI',
       author: 'Adam Lewis',
-      price: 16.99,
+      price: 1899.99,
       img: './assets/covers/book6.jpg'
     ),
     Book(
       id: '7',
       title: 'Shapeshift',
       author: 'Lucas LLoyd',
-      price: 16.99,
+      price: 1699.99,
       img: './assets/covers/book7.jpg'
     ),
     Book(
       id: '8',
       title: 'Harry Potter and The Goblet of Fire',
       author: 'J. K. Rowling',
-      price: 16.99,
+      price: 1499.99,
       img: './assets/covers/book2.jpg'
     ),
   ];
